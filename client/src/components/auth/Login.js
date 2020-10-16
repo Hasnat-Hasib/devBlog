6a920e47253f0b417cbc8add5d740a3b91de34f9
Hasnat <hasib8nh@gmail.com>
2020-10-16 18:23:41 +0600

    React user authentication, load user and set token, user login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, {Fragment, useState} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {login} from '../../actions/auth';
 
-const Login = () => {
+const Login = ({login, isAuthenticated}) => {
     const [formData, setFormData] = useState({
         email: '',
         password: ''
@@ -14,7 +17,13 @@ const Login = () => {
 
     const onSubmit = async e => {
         e.preventDefault();
-        console.log('Success')
+        login( email, password );
+    }
+
+    //Redirect if logged in
+
+    if(isAuthenticated) {
+      return <Redirect to = "/dashboard" />
     }
 
 
@@ -46,5 +55,13 @@ const Login = () => {
   
 }
 
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+}
 
-export default Login;
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, {login}) (Login);
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,11 @@
 import React, {Fragment, useState} from 'react';
 import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import {setAlert} from '../../actions/alert';
+import {register} from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({setAlert}) => {
+const Register = ({ setAlert, register, isAuthenticated}) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -21,9 +22,15 @@ const Register = ({setAlert}) => {
         if (password !== password2) {
             setAlert('Passwords do not match', 'danger');
         }else{
-            console.log('Success');
+            register({name, email, password});
     }
 };
+
+//Redirect if Registered Successfully
+
+if(isAuthenticated) {
+  return <Redirect to = "/dashboard" />
+}
 
   return (
     <Fragment>
@@ -32,11 +39,11 @@ const Register = ({setAlert}) => {
       <form className="form" onSubmit={e => onSubmit(e)} >
         <div className="form-group">
           <input type="text" placeholder="Name" name="name" 
-          value={name} onChange={e => onChange(e)} required />
+          value={name} onChange={e => onChange(e)}  />
         </div>
         <div className="form-group">
           <input type="email" placeholder="Email Address" name="email" 
-          value={email} onChange={e => onChange(e)} required />
+          value={email} onChange={e => onChange(e)}  />
           <small className="form-text"
             >This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small
@@ -48,7 +55,7 @@ const Register = ({setAlert}) => {
             placeholder="Password"
             name="password"
             value={password} onChange={e => onChange(e)}
-            minLength="6"
+            
           />
         </div>
         <div className="form-group">
@@ -57,7 +64,7 @@ const Register = ({setAlert}) => {
             placeholder="Confirm Password"
             name="password2"
             value={password2} onChange={e => onChange(e)}
-            minLength="6"
+           
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
@@ -71,8 +78,15 @@ const Register = ({setAlert}) => {
 }
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+
 }
 
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
 
-export default connect(null, {setAlert}) (Register);
+
+export default connect(mapStateToProps, {setAlert, register}) (Register);
