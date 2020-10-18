2a105bd703d3cee41fad84d51e1c1aa6a0489166
Hasnat <hasib8nh@gmail.com>
2020-10-18 17:35:50 +0600

    Create Profile Action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
-import {setAlart} from './alert';
+import {setAlert} from './alert';
 
-import {GET_PROFILE, PROFILE_ERROR} from './types';
-
+import {
+    GET_PROFILE,
+    GET_PROFILES,
+    PROFILE_ERROR,
+    UPDATE_PROFILE,
+    CLEAR_PROFILE,
+  } from './types'; 
 
 // Get Current user
 
@@ -19,4 +24,41 @@ export const getCurrentProfile = () => async dispatch => {
              payload: {msg: err.response.statusText, status: err.response.status}
         });
     }
-} 
+};
+
+// Create or update profile
+export const createProfile = (
+    formData,
+    history,
+    edit = false
+  ) => async dispatch => {
+
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const res = await axios.post('/api/profile', formData, config);
+        
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+        dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+        if(!edit) {
+            history.push('/dashboard');
+        }
+    } catch (err) {
+        const errors = err.response.data.errors;
+      if(errors){
+          errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+      }
+
+        dispatch({
+            type: PROFILE_ERROR,
+             payload: {msg: err.response.statusText, status: err.response.status}
+        }); 
+        
+    }
+  }
--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,11 +1,12 @@
 import React, { Fragment, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-//import { createProfile, getCurrentProfile } from '../../actions/profile';
+import { createProfile } from '../../actions/profile';
 
 
-const CreateProfile = props => {
+
+const CreateProfile = ({createProfile, history}) => {
     const [formData, setFormData] = useState({
         company: '',
         website: '',
@@ -22,6 +23,7 @@ const CreateProfile = props => {
     });
 
     const [displaySocialInputs, toggleSocialInputs] = useState(false);
+
 
     const {
         company,
@@ -40,6 +42,12 @@ const CreateProfile = props => {
 
     const onChange = e => setFormData ({...formData, [e.target.name] : e.target.value});
 
+    const onSubmit = e => {
+        e.preventDefault();
+        createProfile(formData, history);
+    };
+   
+
     return(
         <Fragment>
         <h1 className="large text-primary">
@@ -50,7 +58,7 @@ const CreateProfile = props => {
             profile stand out
         </p>
         <small>* = required field</small>
-        <form className="form">
+        <form className="form" onSubmit={e => onSubmit(e)} >
             <div className="form-group">
                 <select name="status" value = {status} onChange = {e => onChange(e)}>
                     <option value="0">* Select Professional Status</option>
@@ -152,7 +160,10 @@ const CreateProfile = props => {
 };
 
 CreateProfile.propTypes = {
+    createProfile: PropTypes.func.isRequired
+
 
 }
 
-export default CreateProfile;
+
+export default connect(null, {createProfile}) (withRouter(CreateProfile));
